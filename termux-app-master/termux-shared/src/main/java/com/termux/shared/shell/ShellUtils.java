c1fca5048df64d6ff4741eae4e42c132588ffac6
ghc-dev <haysam.kies+@whitesourcesoftware.com>
2022-03-14 09:45:23 +0200

    sdas

---
+++ termux-app-master/termux-shared/src/main/java/com/termux/shared/shell/ShellUtils.java
@@ -0,0 +1,54 @@
+package com.termux.shared.shell;
+
+import com.termux.shared.file.FileUtils;
+import com.termux.terminal.TerminalBuffer;
+import com.termux.terminal.TerminalEmulator;
+import com.termux.terminal.TerminalSession;
+
+import java.lang.reflect.Field;
+
+public class ShellUtils {
+
+    public static int getPid(Process p) {
+        try {
+            Field f = p.getClass().getDeclaredField("pid");
+            f.setAccessible(true);
+            try {
+                return f.getInt(p);
+            } finally {
+                f.setAccessible(false);
+            }
+        } catch (Throwable e) {
+            return -1;
+        }
+    }
+
+    public static String getExecutableBasename(String executable) {
+        return FileUtils.getFileBasename(executable);
+    }
+
+    public static String getTerminalSessionTranscriptText(TerminalSession terminalSession, boolean linesJoined, boolean trim) {
+        if (terminalSession == null) return null;
+
+        TerminalEmulator terminalEmulator = terminalSession.getEmulator();
+        if (terminalEmulator == null) return null;
+
+        TerminalBuffer terminalBuffer = terminalEmulator.getScreen();
+        if (terminalBuffer == null) return null;
+
+        String transcriptText;
+
+        if (linesJoined)
+            transcriptText = terminalBuffer.getTranscriptTextWithFullLinesJoined();
+        else
+            transcriptText = terminalBuffer.getTranscriptTextWithoutJoinedLines();
+
+        if (transcriptText == null) return null;
+
+        if (trim)
+            transcriptText = transcriptText.trim();
+
+        return transcriptText;
+    }
+
+}
